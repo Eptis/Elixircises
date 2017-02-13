0ad0325e4aa97213b31dfb97758645c3b6cffb7e
Eptis <dennistel90@gmail.com>
2017-02-13 13:35:35 +0100

    finish raindrops assignment

--- elixir/raindrops/raindrops.exs
+++ elixir/raindrops/raindrops.exs
@@ -1,4 +1,5 @@
 defmodule Raindrops do
+  import Enum
   @doc """
   Returns a string based on raindrop factors.
 
@@ -10,6 +11,28 @@ defmodule Raindrops do
   """
   @spec convert(pos_integer) :: String.t
   def convert(number) do
+    factors = map(Range.new(1, number), &_convert(&1, number))
+    |> uniq
+    |> sort
+    |> Enum.filter(&(&1 == 3 || &1 == 5 || &1 == 7))
 
+    case factors do
+      [] -> raindrop(number)
+      list -> raindrop(list)
+    end
+  end
+
+  defp _convert(int, number) do
+    number / int
+  end
+
+  def raindrop(3.0), do: "Pling"
+  def raindrop(5.0), do: "Plang"
+  def raindrop(7.0), do: "Plong"
+  def raindrop(number) when is_number(number), do: Integer.to_string(number)
+  def raindrop(list) when is_list(list) do
+    list
+    |> Enum.map(&(raindrop(&1)))
+    |> List.to_string
   end
 end
